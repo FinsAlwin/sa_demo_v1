a5c2dc07073a819feaed84d264ae3e268fa8fb7f
FinsAlwin <alwinwk101@live.com>
2023-09-04 15:32:23 +0530

    4-9-2023 1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,16 +6,27 @@ import {
   SimpleGrid,
   AbsoluteCenter,
   Text,
-  Center,
+  Button,
   HStack,
-  useColorModeValue,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { FiMenu } from "react-icons/fi";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [arCount, setArCount] = useState(0);
 
   const handleClick = (url) => {
     router.push(`/game-container`);
@@ -23,6 +34,25 @@ export default function Home() {
 
   const handleClickMulti = (url) => {
     router.push(`/multi-player-game-container`);
+  };
+
+  const handleArTest = async (i) => {
+    await setArCount(i);
+  };
+
+  useEffect(() => {
+    if (arCount === 1) {
+      onOpen();
+    } else if (arCount === 2) {
+      onOpen();
+    } else {
+      setArCount(0);
+    }
+  }, [arCount]);
+
+  const handleCloseModal = () => {
+    onClose();
+    setArCount();
   };
 
   return (
@@ -63,6 +93,73 @@ export default function Home() {
           />
         </Box>
       </HStack>
+      <Box position="relative" padding="10">
+        <Divider />
+        <AbsoluteCenter px="4" color="gray.700">
+          AR application
+        </AbsoluteCenter>
+      </Box>
+      <HStack spacing="24px">
+        <Button
+          colorScheme="teal"
+          boxShadow="md"
+          onClick={() => handleArTest(1)}
+        >
+          Cloth Physics Simulation
+        </Button>
+
+        <Button
+          colorScheme="blue"
+          boxShadow="md"
+          onClick={() => handleArTest(2)}
+        >
+          Face Tracking Filter
+        </Button>
+      </HStack>
+
+      <Modal isOpen={isOpen} onClose={handleCloseModal} bg="dark">
+        <ModalOverlay />
+
+        <ModalContent bg="gray.800" color="white">
+          <ModalCloseButton />
+          <ModalHeader>
+            {arCount === 1 && "AR -Cloth Physics Simulation "}
+            {arCount === 2 && "AR - Face Tracking Filter"}
+          </ModalHeader>
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              Scan QR code using phone
+            </Text>
+            {arCount === 1 && (
+              <Image
+                boxShadow="md"
+                src="/image/ar_test_moon.png"
+                width={400}
+                height={400}
+                style={{ borderRadius: "20%" }}
+                alt="profile pic"
+              />
+            )}
+
+            {arCount === 2 && (
+              <Image
+                boxShadow="md"
+                src="/image/ar_face_tracking.png"
+                width={400}
+                height={400}
+                style={{ borderRadius: "20%" }}
+                alt="profile pic"
+              />
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleCloseModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Admin>
   );
 }
